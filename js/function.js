eec20432f086b3800428ae1cec4280e1f4baa1c4
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-19 18:42:51 +0100

    :)

---
+++ js/function.js
@@ -0,0 +1,3 @@
+function pluto(difficolta, boxes){
+    
+}
--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-const boxes = document.querySelector('.box');
-let numero;
+const boxes = document.querySelector('.container-griglia');
+const bottone = document.getElementById('play');
+const difficolta = document.getElementById('difficolta');
 
+play.addEventListener('click', function(){
+    
+    for (let i = 1; i <= 100; i++){
+         const cardElement= document.createElement('div');
+        cardElement.classList.add('box');
+        cardElement.innerHTML = i;
+    
+        cardElement.addEventListener('click', function(){
+            cardElement.className = 'box active';
+        });
+    
+        boxes.append(cardElement);
+    }
+})
 
-
-for (let i = 1; i <=100; i++){
- numero = [i];
- const card = `<div class="box ">${numero}</div>`;
- boxes.innerHTML += card;
- console.log(i);
-}
